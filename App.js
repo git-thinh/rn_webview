c31640c169fc68aabd2f01664f1ca804edf4139a
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2023-04-19 07:15:05 +0700

    3535

--- App.js
+++ App.js
@@ -7,10 +7,14 @@ import {
 	NativeBaseProvider,
 	Box,
 	Button,
+	Text,
 } from "native-base";
 
 import { WebView } from 'react-native-webview';
+import GestureRecognizer, {swipeDirections} from './src/swipe-gestures.js';
 
+const HOST = 'http://192.168.1.50'
+		
 export default class App extends React.Component {	
 	constructor(props) {
 		super(props);
@@ -23,23 +27,37 @@ export default class App extends React.Component {
 			userAgent: '',
 			injectedJSBeforeLoaded: '',
 			injectedJS: '',
+			
+			gestureName: '',
 		};
 	
 	}	
 	
 	componentDidMount() {
-		const self = this;
 		StatusBar.setHidden(true);
-		
-		fetch('http://192.168.1.50/test.html').then(r=>r.text()).then(s=>{
-			self.setState({html:s});
-		})
+		this.loadHtml();
 	}
 	
+	onSwipe(gestureName, gestureState) {
+		const {SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT} = swipeDirections;
+		this.setState({gestureName: gestureName});		
+		this.webref.injectJavaScript(`if(window['__onSWIPE'])window['__onSWIPE']('${gestureName}');true;`);
+	}
+	onTab() {
+		const gestureName = 'TAB';
+		this.setState({gestureName: gestureName});		
+		this.webref.injectJavaScript(`if(window['__onSWIPE'])window['__onSWIPE']('${gestureName}');true;`);
+	}
+		
 	render(){		
+    const config = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 80
+    };
+
 		return (
 			<NativeBaseProvider>
-				<Box w="100%" h="100%">
+				<Box w="100%" h="100%" style={{}}>
 					<WebView
 						//renderLoading={LoadingSpinner}
 						
@@ -65,16 +83,39 @@ export default class App extends React.Component {
 						style={styles.webview}
 					/>
 					<Button style={styles.button} onPress={this.testPress}>Test</Button>
+					<GestureRecognizer
+						onSwipe={(direction, state) => this.onSwipe(direction, state)}
+						tab={this.onTab}
+						config={config}
+						style={styles.gestureRecognizer}>
+						<Button style={styles.loadHtml} onPress={this.loadHtml}>R</Button>
+						<Text>{this.state.gestureName}</Text>
+					</GestureRecognizer>
 				</Box>
 			</NativeBaseProvider>
 		);
 	}
 	
+	loadHtml = () => {
+		console.log('LOAD_HTML....');
+		const self = this;
+		const time = '?_=' + new Date().getTime().toString();
+		fetch(`${HOST}/test.html`+time).then(r=>r.text()).then(html=>{
+			fetch(`${HOST}/test.js`+time).then(r=>r.text()).then(js=>{
+				fetch(`${HOST}/test.css`+time).then(r=>r.text()).then(css=>{
+					const data = '<style type="text/css">'+css+'</style>' + 
+						html + 
+						'<script>'+js+'</script>';
+					self.setState({html:data});
+				})
+			})
+		})
+	}
+	
 	////////////////////////////////////////////////////////////////////////////////
 	testPress = () => {		
 		console.log('TEST_PRESS....');
-		let js = '';
-		
+		let js = '';		
 		js = `; 
 			let s = "?????????? testPress = " + window.isNativeApp;
 			window.ReactNativeWebView.postMessage(s);			
@@ -97,6 +138,17 @@ const styles = StyleSheet.create({
   },
   webview: {
   },
+  loadHtml: {
+		width: 32,
+  },
+	gestureRecognizer: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 70,
+		bottom: 0,
+		//backgroundColor: 'red'
+	},
 	button: {
 		position: 'absolute',
 		fontSize: 13,
